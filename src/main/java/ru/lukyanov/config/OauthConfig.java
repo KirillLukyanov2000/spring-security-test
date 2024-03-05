2411589b5225f9aea7a5e9c7993df581333c1cc0
Kirill Lukyanov <kirs2000@mail.ru>
2024-03-05 09:45:24 +0300

    Oauth2 Config added

---
+++ src/main/java/ru/lukyanov/config/OauthConfig.java
@@ -0,0 +1,24 @@
+package ru.lukyanov.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class OauthConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/index").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login();
+        return http.build();
+    }
+
+}
