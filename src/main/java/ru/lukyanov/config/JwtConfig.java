fad14c766627115fd50a6b311686edbc171a8d36
Kirill Lukyanov <101703819+KirillLukyanov2000@users.noreply.github.com>
2024-04-02 12:38:34 +0300

    Update JwtConfig.java

--- src/main/java/ru/lukyanov/config/JwtConfig.java
+++ src/main/java/ru/lukyanov/config/JwtConfig.java
@@ -1,1 +1,13 @@
 
+@Configuration
+@EnableWebSecurity
+public class JwtConfig {
+
+  @Autowired
+  private JwtAuthenticationEntryPoint entryPoint;
+  @Autowired
+  private UserDetailsService userDetailsService;
+  @Autiwired
+  private JwtRequestFilter requestFilter;
+
+}
