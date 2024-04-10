a8606c89501c507585f6c11008ffd551051247e3
Kirill Lukyanov <kirs2000@mail.ru>
2024-04-10 09:33:54 +0300

    JWT Configuration added

--- src/main/java/ru/lukyanov/config/JwtConfig.java
+++ src/main/java/ru/lukyanov/config/JwtConfig.java
@@ -1,13 +1,60 @@
+package ru.lukyanov.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ru.lukyanov.config.jwt.JwtRequestFilter;
+
 
 @Configuration
 @EnableWebSecurity
 public class JwtConfig {
 
-  @Autowired
-  private JwtAuthenticationEntryPoint entryPoint;
-  @Autowired
-  private UserDetailsService userDetailsService;
-  @Autiwired
-  private JwtRequestFilter requestFilter;
+    @Autowired
+    private AuthenticationProvider authenticationProvider;
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private JwtRequestFilter requestFilter;
 
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/v1/auth/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(requestFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 }
+
+
